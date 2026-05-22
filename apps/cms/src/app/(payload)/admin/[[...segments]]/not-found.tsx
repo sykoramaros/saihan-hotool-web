62e1b6ae140c235526c3b307afe50e33d037bcfb
Marosh Sykora <sykoramaros@gmail.com>
2026-05-22 23:07:54 +0200

    fix: add importMap to CMS not-found page (Payload 3.x compatibility)

--- apps/cms/src/app/(payload)/admin/[[...segments]]/not-found.tsx
+++ apps/cms/src/app/(payload)/admin/[[...segments]]/not-found.tsx
@@ -2,6 +2,7 @@
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import config from '@payload-config'
 import { NotFoundPage } from '@payloadcms/next/views'
+import { importMap } from '../importMap'
 
 type Args = {
   params: Promise<{ segments: string[] }>
@@ -9,6 +10,6 @@ type Args = {
 }
 
 const NotFound = ({ params, searchParams }: Args) =>
-  NotFoundPage({ config, params, searchParams })
+  NotFoundPage({ config, params, searchParams, importMap })
 
 export default NotFound
